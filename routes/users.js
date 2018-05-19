9ac165ad8d4eb15a25502b2c176e26fecc23eaf9
宋龙飞 <songlongfei@ccmtjf.com>
2018-05-19 23:49:37 +0800

    this is note test

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource changed remote');
+  res.send('respond with a resource changed remote add note');
 });
 
 module.exports = router;
